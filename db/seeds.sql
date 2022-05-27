49703c157962f23f999237895337af574acafa56
katelynarmstrong <katelynarmstrong6@gmail.com>
2022-05-27 17:08:20 -0500

    initial db set up

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+('Katelyn', 'Armstrong', 1, 1),
+('Matthew', 'Ralph', 1, 1),
+('Lola', 'Goober', 0, 2),
+('Tazi', 'Raz', 1, 2),
+('Nitro', 'Sexton', 1, 1);
