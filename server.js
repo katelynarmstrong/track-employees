ccac6a43ff0664f7cb2ead53d4c9baf0b6344099
katelynarmstrong <katelynarmstrong6@gmail.com>
2022-05-27 17:35:44 -0500

    server updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mysql = require('mysql2');
-const apiRoutes = require('./routes/apiRoutes');
+const express = require("express");
+const mysql = require("mysql2");
+const apiRoutes = require("./routes/apiRoutes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,12 +9,19 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/api',apiRoutes);
+app.use("/api", apiRoutes);
 
-// KEEP AT BOTTOM OF SERVER.JS
+// Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
-  });
-app.listen(PORT, () => {
+  res.status(404).end();
+});
+
+// Start server after DB connection
+//KEEP AT BOTTOM OF SERVER.JS
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected.");
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+});
